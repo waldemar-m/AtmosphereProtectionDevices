2d5272b9d8291457371eb88a02c4feb19d4406cc
janko <molo226@vp.pl>
2018-12-05 19:35:26 +0100

    Fixes after rebase

--- wyswietl/forms.py
+++ wyswietl/forms.py
@@ -11,46 +11,25 @@ class OdbiorcaForm(forms.ModelForm):
         field='__all__'
         exclude = ()
 
+
 class ParametryForm(forms.ModelForm):
        
      class Meta:
        model=Parametry
-<<<<<<< HEAD
-<<<<<<< HEAD
-       field=('wysokosc','obroty','strumien',)
-=======
        field='__all__'
        exclude = ()
->>>>>>> 6deb9f7... Fixing after migrations from remote repository
-=======
-       field='__all__'
-       exclude = ()
->>>>>>> 6deb9f7... Fixing after migrations from remote repository
+
 
 class UOAP0Form(forms.ModelForm):
      
      class Meta:
          model=UOAP0
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+         field='__all__'
          exclude = ()
->>>>>>> 6deb9f7... Fixing after migrations from remote repository
-=======
-         exclude = ()
->>>>>>> 6deb9f7... Fixing after migrations from remote repository
+
 
 class UOAP2Form(forms.ModelForm):
      
     class Meta:
-<<<<<<< HEAD
-<<<<<<< HEAD
-        model=UOAP2         
-=======
         model=UOAP2     
         exclude = ()
->>>>>>> 6deb9f7... Fixing after migrations from remote repository
-=======
-        model=UOAP2     
-        exclude = ()
->>>>>>> 6deb9f7... Fixing after migrations from remote repository
